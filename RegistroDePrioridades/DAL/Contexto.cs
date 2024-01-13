bd35a982a3b68e59e115ad2638a93197d78e1753
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-01-12 21:27:22 -0400

    Creando la carpeta DAL, la cual la cual contiene nuestra clase contexto

---
+++ RegistroDePrioridades/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroDePrioridades.Models;
+
+namespace RegistroDePrioridades.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Prioridades> Prioridades { get; set; }
+
+    }
+}
