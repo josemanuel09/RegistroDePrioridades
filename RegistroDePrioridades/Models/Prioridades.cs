89ea14778824cf519a796a035f7e23b0297266ec
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-01-12 21:20:28 -0400

    Creando la carpeta Models, la cual contiene la clase prioridades

---
+++ RegistroDePrioridades/Models/Prioridades.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDePrioridades.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "El descripcion es obligatorio")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Los dias de comprimiso deben ser validos")]
+        [Range(1, 31, ErrorMessage = "Los dias de comprimiso deben ser validos")]
+        public int DiasCompromiso { get; set; }
+    }
+}
