8924820a31a9038c6d7897e866af272551213c28
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-01-22 15:02:20 -0400

    Haciendo las ultimas modificaciones, y agragando los datos a la base de datos

--- RegistroDePrioridades/Models/Clientes.cs
+++ RegistroDePrioridades/Models/Clientes.cs
@@ -18,30 +18,15 @@ namespace RegistroDePrioridades.Models
         [StringLength(9, ErrorMessage = "No Puede Exceder los 9 Caracteres")]
         [Required(ErrorMessage = "El Campo Rnc es Obligatorio")]
         public string? Rnc { get; set; }
-        [Required(ErrorMessage = "El Campo Email es obligatorio")]
-        [StringLength(30, ErrorMessage = "No Puede Exceder los 30 Caracteres")]
+        [Required(ErrorMessage = "El campo email es Obligatoeio")]
         [EmailAddress(ErrorMessage = "El formato del email no es Valido")]
-        [ArrobaValidation(ErrorMessage = "El Email Debe Contener @")]
+        
         public string? Email { get; set; }
         [Required(ErrorMessage = "El campo Direccion es Obligatoeio")]
         [StringLength(50, ErrorMessage = "No Puede Exceder los 50 Caracteres")]
         public string? Direccion { get; set; }
     }
 
-    public class ArrobaValidation : ValidationAttribute
-    {
-        protected override ValidationResult? IsValid(object? valor, ValidationContext validationContext)
-        {
-            if (valor != null)
-            {
-                string email = valor.ToString();
-                if (!email.Contains("@"))
-                    return ValidationResult.Success;
-                else
-                    return new ValidationResult("El Email Debe Contener @");
-            }
-            return new ValidationResult("Debes Colocar un Email");
-        }
-    }
+    
 }
 
