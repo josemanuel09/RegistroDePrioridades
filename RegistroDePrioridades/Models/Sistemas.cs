78a6e90bd35555aa8b3d59353431462795da30b6
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-01-29 00:33:42 -0400

    Creando modelo de sistemas

---
+++ RegistroDePrioridades/Models/Sistemas.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDePrioridades.Models
+{
+    public class Sistemas
+    {
+       
+            [Key]
+            public int SistemaId { get; set; }
+
+            [Required(ErrorMessage = "Campo Obligatorio")]
+            public string? NombreSistema { get; set; }
+    }
+}
